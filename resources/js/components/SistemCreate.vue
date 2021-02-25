ad9df7d4668509da9b35afec2e8af458f5f00bee
PC <orhanerday@gmail.com>
2021-02-25 17:43:52 +0300

    toast eklendi

--- resources/js/components/SistemCreate.vue
+++ resources/js/components/SistemCreate.vue
@@ -3,34 +3,41 @@
             <!-- left column -->
             <div class="col-md-12">
                 <!-- general form elements -->
-                <div class="card">
+                <form @submit.prevent="storeSistem">
+                    <div class="card">
                     <div class="card-header">
                         <h3 class="card-title">
                             Sistem - Yeni Oluştur
                         </h3>
                     </div>
                         <div class="card-body">
+
                             <div class="row">
 
 
                                 <div class="form-group col-md-6">
                                     <label>İl</label>
                                     <select class="form-control select2" required data-placeholder="İl Seçiniz" v-select2  v-model="selected_il">
-                                        <option v-for="il in iller" :value="il.id">{{ il.ad}}</option>
+                                        <option  v-for="il in iller" :value="il.id">{{ il.ad}}</option>
                                     </select>
                                 </div>
 
                                 <div class="form-group col-md-6">
                                     <label>İlçe</label>
-                                    <select class="form-control" required data-placeholder="İlçe Seçiniz">
-                                        <option v-for="ilce in ilceler" :value="ilce.id">{{ ilce.ad}}</option>
+                                    <select class="form-control" required data-placeholder="İlçe Seçiniz" v-model="formData.ilce_id">
+                                        <option  v-for="ilce in ilceler" :value="ilce.id">{{ ilce.ad}}</option>
                                     </select>
                                 </div>
 
 
-                                <div class="form-group col-md-12">
+                                <div class="form-group col-md-6">
                                     <label>Sistem Adı</label>
-                                    <input type="text" required name="name" class="form-control">
+                                    <input ref="clear" type="text" required name="ad" class="form-control" v-model="formData.ad">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label>Telefon Numarası</label>
+                                    <input ref="clear" type="text" v-mask="'0 (###) ###-####'" required name="telefon" class="form-control" v-model="formData.telefon">
+
                                 </div>
 
 
@@ -43,6 +50,7 @@
                             <button type="submit" class="btn btn-primary float-right">Kaydet</button>
                         </div>
                 </div>
+                </form>
                 <!-- /.card -->
 
             </div>
@@ -50,13 +58,22 @@
 </template>
 
 <script>
+import {TheMask} from 'vue-the-mask'
     export default {
-
+        components: {TheMask},
         data: function () {
             return {
                 iller: [],
                 ilceler: [],
-                selected_il:''
+                selected_il:'',
+
+                formData: {
+                    ilce_id: '',
+                    il_id: '',
+                    ad:'',
+                    telefon:''
+                }
+
             }
         },
 
@@ -68,7 +85,23 @@
         },
 
         methods: {
+            storeSistem(event){
 
+                axios.post('/api/sistem/store',this.formData)
+                    .then((response)=>{
+                        Vue.$toast.success('Kayıt Başarı İle Eklendi!.', {
+                            // override the global option
+                            position: 'top-right'
+
+
+                        })
+                       console.log(response.data)
+                        event.target.reset();
+
+                    }).catch(function (error) {
+                    console.log(error);
+                });
+            },
             getIl(){
 
                 axios.get('/api/il')
@@ -102,8 +135,9 @@
 
         watch : {
             selected_il(val) {
+                this.formData.il_id = val
                 this.getIlce(val)
-            },
+            }
         }
 
 
